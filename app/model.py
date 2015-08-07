6088327d444082c286b5d2808891ffe1965bbd66
Piotr Sliwa <psliwa@pl-byd-srv01.emea.int.genesyslab.com>
2015-08-07 12:09:27 +0200

    User class - add jira credentials to db

--- app/model.py
+++ app/model.py
@@ -35,6 +35,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     cc_login = db.Column(db.String(50))
+    jira_login = db.Column(db.String(255), unique=True)
+    jira_password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=roles_users,
