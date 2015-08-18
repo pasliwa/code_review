7faab3ad3c660db3f6dd90fa5cd565506283c01a
Piotr Sliwa <psliwa@pl-byd-srv01.emea.int.genesyslab.com>
2015-08-18 12:31:29 +0200

    Relocate db_archive.py

--- db_archive.py
+++ db_archive.py
@@ -5,13 +5,14 @@ archiver_login = 'abc'
 archiver_password = 'xyz'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///testing.db"
 db = SQLAlchemy(app)
 
 db.create_all()
 
-from app.model import Changeset, Review
+from app.model import Changeset, Review, User
 
+    
 from app.jira import integrate_all_old
 from app.crypto import encryption
 
