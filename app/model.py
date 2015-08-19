8973a60107941722f3d5e13808eec0d5dce803af
Piotr Sliwa <psliwa@pl-byd-srv01.emea.int.genesyslab.com>
2015-08-19 11:16:55 +0200

    Add name column to user table

--- app/model.py
+++ app/model.py
@@ -32,6 +32,7 @@ class Role(db.Model, RoleMixin):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     cc_login = db.Column(db.String(50))
