21584df16dd8b21564dcd8ed53dee462efcbbdb6
Piotr Sliwa <psliwa@pl-byd-srv01.emea.int.genesyslab.com>
2015-09-21 16:47:50 +0200

    Bookmark_cleanup - fixes (no changes found, bookmark doesn't exist)

--- bookmark_cleanup.py
+++ bookmark_cleanup.py
@@ -10,19 +10,25 @@ db.create_all()
 
 from app.mercurial import Repo
 
+
 repo = Repo("../repository/project_1")
 
 from app.model import Changeset, Review, User
 
 merged_reviews = Review.query.filter(Review.status == "MERGED").all()
+bookmarks = repo.hg_bookmarks()
+
 
 for merged_review in merged_reviews:
-    repo.hg_bookmark(bookmark=merged_review.bookmark, delete=True)
+    if merged_review.bookmark in bookmarks:
+        repo.hg_bookmark(bookmark=merged_review.bookmark, delete=True)
     
 abandoned_reviews = Review.query.filter(Review.status == "ABANDONED").all()
 
-for abandoned_review in abandoned_reviews:
-    repo.hg_bookmark(bookmark=abandoned_review.bookmark, delete=True)
-    
 
+for abandoned_review in abandoned_reviews:
+    if abandoned_review.bookmark in bookmarks:
+        repo.hg_bookmark(bookmark=abandoned_review.bookmark, delete=True)
+    
+repo.hg_commit("Bookmark cleanup")
 repo.hg_push()
